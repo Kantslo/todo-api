bb846b26f58852aa3185e914f959ff8ae116d7a4
Giorgi Kantsliani <gqancliani@gmail.com>
2023-12-14 03:22:33 +0400

    added gitignore file

---
+++ src/config/sql.js
@@ -0,0 +1,17 @@
+import pgk from "pg";
+
+const { Pool } = pgk;
+
+const pool = new Pool({
+  host: "dpg-clt1cgqpmc4c73duu070-a",
+  port: 5432,
+  database: "todo_tt1y",
+  user: "todo_tt1y_user",
+  password: "jG37r2FGIDuR6eIvDDTLpE0IPSQW19PY",
+});
+
+export const createTable = async () => {
+  return await pool.query(
+    "CREATE TABLE IF NOT EXISTS todo (id SERIAL PRIMARY KEY, task TEXT, completed BOOLEAN)"
+  );
+};
--- src/server.js
+++ src/server.js
@@ -1,14 +1,27 @@
 import express from "express";
 import api from "./api/index.js";
 import cors from "cors";
+import { createTable } from "./config/sql.js";
 
 const app = express();
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Welcome to home page" });
-});
+const init = async () => {
+  try {
+    await createTable();
+    serverStart();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const serverStart = () => {
+  app.get("/", (req, res) => {
+    return res.status(200).json({ message: "Welcome to home page" });
+  });
+  app.listen(3001);
+};
 
 app.use("/api", api);
 
-app.listen(3001);
+init();
