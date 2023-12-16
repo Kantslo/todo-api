b1f26552831cdd2569a47bdd8001f4a8008ef2e3
Giorgi Kantsliani <gqancliani@gmail.com>
2023-12-16 23:51:07 +0400

    running tests

--- src/routes/task-router.js
+++ src/routes/task-router.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { createTask, getAllTasks } from "../controllers/tasks-controller.js";
+import { createTask, getAllTasks } from "../controllers/task-controller.js";
 
 const taskRouter = express.Router();
 
-taskRouter.get("/tasks", getAllTasks);
-taskRouter.post("/tasks", createTask);
+taskRouter.get("/tasks", getAllTasks());
+taskRouter.post("/tasks", createTask());
 
 export default taskRouter;
