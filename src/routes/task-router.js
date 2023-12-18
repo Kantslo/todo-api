57a60a42b008a15bf41cdb0620ee8e30ce3858a8
Giorgi Kantsliani <gqancliani@gmail.com>
2023-12-18 19:00:23 +0400

    added delete task file

--- src/routes/task-router.js
+++ src/routes/task-router.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { createTask, getAllTasks } from "../controllers/task-controller.js";
+import {
+  createTask,
+  deleteTask,
+  getAllTasks,
+} from "../controllers/task-controller.js";
 
 const taskRouter = express.Router();
 
 taskRouter.get("/tasks", getAllTasks);
 taskRouter.post("/tasks", createTask);
-taskRouter.delete("/tasks/:id");
+taskRouter.delete("/tasks/:id", deleteTask);
 
 export default taskRouter;
