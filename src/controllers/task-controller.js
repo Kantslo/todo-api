1e696f7f16b7fb0c13a93ec948bd277a444650c5
Giorgi Kantsliani <gqancliani@gmail.com>
2023-12-20 17:49:12 +0400

    delete response fix

--- src/controllers/task-controller.js
+++ src/controllers/task-controller.js
@@ -40,7 +40,9 @@ export const deleteTask = async (req, res) => {
 
 export const deleteCompletedTasks = async (_, res) => {
   try {
-    await pool.query("DELETE FROM todos WHERE completed = true");
+    const resultQuery = await pool.query(
+      "DELETE FROM todos WHERE completed = true"
+    );
     const rows = resultQuery.rows;
     return res.status(201).json(rows[0]);
   } catch (error) {
